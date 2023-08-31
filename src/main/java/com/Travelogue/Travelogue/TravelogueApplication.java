32bf4aecbc38a4a9f7b725f5622d1713bd466e6e
ikk5515 <ikk5515@naver.com>
2023-09-01 00:29:55 +0900

    초기 Spring boot 세팅파일

---
+++ src/main/java/com/Travelogue/Travelogue/TravelogueApplication.java
@@ -0,0 +1,13 @@
+package com.Travelogue.Travelogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Travelogue/Travelogue/TravelogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Travelogue.Travelogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
