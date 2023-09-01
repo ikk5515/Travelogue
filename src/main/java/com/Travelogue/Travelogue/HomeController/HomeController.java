b4f5982dd14a8d558a67f52a0584cdd74a593032
ikk5515 <ikk5515@naver.com>
2023-09-02 00:37:14 +0900

    CI/CD 테스트용 파일

---
+++ src/main/java/com/Travelogue/Travelogue/HomeController/HomeController.java
@@ -0,0 +1,13 @@
+package com.Travelogue.Travelogue.HomeController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
