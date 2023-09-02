029a179f02be6799984c0a1b8101173c482800cd
ikk5515 <ikk5515@naver.com>
2023-09-03 00:48:21 +0900

    main관련 디렉토리 세분화 및 Controller 생성

--- src/main/java/com/Travelogue/Travelogue/Main/Controller/mainController.java
+++ src/main/java/com/Travelogue/Travelogue/Main/Controller/mainController.java
@@ -1,13 +1,13 @@
-package com.Travelogue.Travelogue.HomeController;
+package com.Travelogue.Travelogue.Main.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class mainController {
 
     @GetMapping("/")
     public String home() {
-        return "home";
+        return "main";
     }
 }
