57f9b28f0a0c5724cb5465d2b1dfbbdbf32dfba6
hanyongjun <gksdydwns34@naver.com>
2023-09-26 19:11:30 +0900

    login 페이지 기본 틀 & 디자인 작업 30% 완료 & 애니메이션 85%완료

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,16 @@
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
+
+
+function login(){
+    x.style.left = "550px";
+    y.style.left = "-200px";
+    z.style.left = "0";
+}
+
+function register(){
+    x.style.left = "-380px";
+    y.style.left = "550px";
+    z.style.left = "120px";
+}
