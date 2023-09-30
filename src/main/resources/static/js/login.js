fffc4afa88e9d0047e64078063203ff45d76e288
hanyongjun <gksdydwns34@naver.com>
2023-10-01 01:23:49 +0900

    login mobile용 미디어 쿼리 적용 & js 애니메이션 좌표 수정 &login.html파일 경로 수정

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -2,15 +2,37 @@ var x = document.getElementById("login");
 var y = document.getElementById("register");
 var z = document.getElementById("btn");
 
+if(matchMedia("screen and (min-width:1024px)").matches){
+    
+    console.log("desktop");   
 
-function login(){
-    x.style.left = "550px";
-    y.style.left = "-300px";
-    z.style.left = "0";
+    function login(){
+        x.style.left = "550px";
+        y.style.left = "-300px";
+        z.style.left = "0";
+    }
+
+    function register(){
+        x.style.left = "-550px";
+        y.style.left = "545px";
+        z.style.left = "105px";
+    }
+}else if(matchMedia("screen and (max-width:757px)").matches){
+    console.log("moblie");    
+    
+    function login(){
+        x.style.left = "20px";
+        y.style.left = "-500px";
+        z.style.left = "0";
+    }
+
+    function register(){
+        x.style.left = "-500px";
+        y.style.left = "20px";
+        z.style.left = "70px";
+    }
 }
 
-function register(){
-    x.style.left = "-550px";
-    y.style.left = "545px";
-    z.style.left = "105px";
-}
+window.onresize = function(){
+    document.location.reload();
+  };
